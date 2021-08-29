89fb03751047ec8de90498793c52ab5645f7992d
Ios Kateyka <ioskateyka@gmail.com>
2021-08-29 09:35:42 +0300

    Initial Commit

---
+++ MobileDataHouseTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileDataHouseTest
+//
+//  Created by Екатерина Боровкова on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
