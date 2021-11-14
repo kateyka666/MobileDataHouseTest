30b81331704c2ba72af0f133d79ea2feef7b5f37
Ios Kateyka <ioskateyka@gmail.com>
2021-11-14 12:02:23 +0300

    rename project

--- MobileDataHouse/Controllers/StartViewController.swift
+++ MobileDataHouse/Controllers/StartViewController.swift
@@ -16,7 +16,7 @@ class StartViewController: UIViewController {
         super.viewDidLoad()
         requestTextField.delegate = self
     }
-    
+ 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
@@ -31,14 +31,14 @@ class StartViewController: UIViewController {
             present(newViewController, animated: false, completion: nil)
            }
     }
-    
+   
 
 }
 extension StartViewController : UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let allowedCharacter = CharacterSet.letters
-        let characterSet = CharacterSet(charactersIn: string)
-        return allowedCharacter.isSuperset(of: characterSet)
+        let characterSets = CharacterSet(charactersIn: string)
+        return allowedCharacter.isSuperset(of: characterSets)
     }
 }
--- MobileDataHouse/Managers/ImageManager.swift
+++ MobileDataHouse/Managers/ImageManager.swift
@@ -12,7 +12,7 @@ class ImageManager {
     static let shared = ImageManager()
     
     private init() {}
-    
+
     func fetchImage(for request : String, completionHandler: @escaping (ImageFromAPI) -> Void) {
         
         let apiString = "https://api.unsplash.com/search/photos?client_id=SFricHxZ7Zm1DWXC6oHmZ1rX2zPogSNJP2SmA-Gz6Rc&page=3&query=\(request)"
